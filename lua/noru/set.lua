cfcff4e31de12ee53a92e67c1224e2b2d8e4e127
Jedsam <fgokgoz02@hotmail.com>
2025-03-30 11:14:00 +0300

    and removed ~ character in the text editor

--- lua/noru/set.lua
+++ lua/noru/set.lua
@@ -3,3 +3,8 @@ vim.opt.tabstop = 4 -- Amount of spaces a \t character represent
 vim.opt.softtabstop = 4 -- Amount of spaces when pressing TAB or Backspace 
 vim.opt.shiftwidth = 4 -- Amount of spaces used for indentation 
 vim.opt.expandtab = true -- Converts \t into spaces when pressing TAB
+
+-- Enable relative line numbers
+vim.opt.nu = true
+vim.opt.rnu = true
+vim.opt.fillchars:append { eob = " " }
