68d0721a7c6d48ca46ea3c987cada7fe75b321cc
Jedsam <fgokgoz02@hotmail.com>
2025-04-02 15:27:07 +0300

    added line to mute jdlts outputs in noice and changed LspLog level to off

--- lua/noru/plugins/lsp/lsp-config.lua
+++ lua/noru/plugins/lsp/lsp-config.lua
@@ -1,11 +1,12 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {} )
-        end
-    }
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {} )
+      vim.lsp.set_log_level("off")
+    end
+  }
 }
 
