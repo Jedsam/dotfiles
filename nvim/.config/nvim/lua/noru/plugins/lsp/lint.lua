41715312c037d9b26fadf1dda4dd5880c884a719
Jedsam <fgokgoz02@hotmail.com>
2025-05-07 17:47:05 +0300

    added filter for cpplint

--- nvim/.config/nvim/lua/noru/plugins/lsp/lint.lua
+++ nvim/.config/nvim/lua/noru/plugins/lsp/lint.lua
@@ -1,26 +1,31 @@
 return {
-    {
-        "mfussenegger/nvim-lint",
-        config = function()
-            local linter = require("lint")
-            -- Run vim.bo.filetype to get the filetype of a file
-            linter.linters_by_ft = {
-                c = { 'cpplint', 'cppcheck' },
-                cmake = { 'cmakelint' },
-                markdown = { 'vale' },
-            }
-            -- Lint on event
-            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-                callback = function()
-                    -- try_lint without arguments runs the linters defined in `linters_by_ft`
-                    -- for the current filetype
-                    linter.try_lint()
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			local linter = require("lint")
+			-- Run vim.bo.filetype to get the filetype of a file
+			linter.linters_by_ft = {
+				c = { "cpplint", "cppcheck" },
+				cmake = { "cmakelint" },
+				markdown = { "vale" },
+			}
+			-- Configuring the existing linters
+			local cpplint = linter.linters.cpplint
+			cpplint.args = {
+				"--filter=-whitespace/line_length,-build/include_subdir,-legal/copyright",
+			}
+			-- Lint on event
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					-- try_lint without arguments runs the linters defined in `linters_by_ft`
+					-- for the current filetype
+					linter.try_lint()
 
-                    -- You can call `try_lint` with a linter name or a list of names to always
-                    -- run specific linters, independent of the `linters_by_ft` configuration
-                    -- require("lint").try_lint("cspell")
-                end,
-            })
-        end
-    },
+					-- You can call `try_lint` with a linter name or a list of names to always
+					-- run specific linters, independent of the `linters_by_ft` configuration
+					-- require("lint").try_lint("cspell")
+				end,
+			})
+		end,
+	},
 }
