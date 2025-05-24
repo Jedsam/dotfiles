7d7a7d561d7a028d792c7daf51dbf0fb0ef83cb5
Jedsam <fgokgoz02@hotmail.com>
2025-05-25 01:25:56 +0300

    disabled grapple and replaced it with harpoon

--- nvim/.config/nvim/lua/noru/plugins/grapple.lua
+++ nvim/.config/nvim/lua/noru/plugins/grapple.lua
@@ -1,19 +1,18 @@
 return {
-    "cbochs/grapple.nvim",
-    opts = {
-        scope = "git", -- also try out "git_branch"
-        icons = false, -- setting to "true" requires "nvim-web-devicons"
-        status = false,
-    },
-    keys = {
-        { "<leader>a", "<cmd>silent Grapple toggle<cr>", desc = "Tag a file" },
-        { "<c-e>", "<cmd>silent Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+	"cbochs/grapple.nvim",
+	opts = {
+		scope = "git", -- also try out "git_branch"
+		icons = false, -- setting to "true" requires "nvim-web-devicons"
+		status = false,
+	},
+	keys = {
+		{ "<leader>a", "<cmd>silent Grapple toggle<cr>", desc = "Tag a file" },
+		{ "<c-e>", "<cmd>silent Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
 
-        { "<c-h>", "<cmd>silent Grapple select index=1<cr>", desc = "Select first tag" },
-        { "<c-j>", "<cmd>silent Grapple select index=2<cr>", desc = "Select second tag" },
-        { "<c-k>", "<cmd>silent Grapple select index=3<cr>", desc = "Select third tag" },
-        { "<c-l>", "<cmd>silent Grapple select index=4<cr>", desc = "Select fourth tag" },
-
-    },
+		{ "<c-h>", "<cmd>silent Grapple select index=1<cr>", desc = "Select first tag" },
+		{ "<c-j>", "<cmd>silent Grapple select index=2<cr>", desc = "Select second tag" },
+		{ "<c-k>", "<cmd>silent Grapple select index=3<cr>", desc = "Select third tag" },
+		{ "<c-l>", "<cmd>silent Grapple select index=4<cr>", desc = "Select fourth tag" },
+	},
+	enabled = false,
 }
-    
