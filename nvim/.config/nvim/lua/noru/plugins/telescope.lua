9a8757b14b9ccae3a796b5751b8d6455eeba0e3f
Jedsam <fgokgoz02@hotmail.com>
2025-08-09 20:44:41 +0300

    telescope size adjustments

--- nvim/.config/nvim/lua/noru/plugins/telescope.lua
+++ nvim/.config/nvim/lua/noru/plugins/telescope.lua
@@ -9,14 +9,11 @@ return {
 					sorting_strategy = "ascending",
 					layout_strategy = "horizontal",
 					layout_config = {
-						width = 0.95,
 						preview_width = 0.7,
+						width = 0.95,
 						height = 0.9,
 						prompt_position = "bottom",
 						preview_cutoff = 20,
-						preview_height = function(_, _, max_lines)
-							return max_lines - 15
-						end,
 					},
 					hidden = true,
 					no_ignore = true,
@@ -35,15 +32,48 @@ return {
 					find_command = { "rg", "--files", "-g", "!.git" },
 				})
 			end, { noremap = true, silent = true })
+			-- No preview project finder
+			vim.keymap.set("n", "<leader>pF", function()
+				require("telescope.builtin").find_files({
+					hidden = false,
+					find_command = { "rg", "--files", "-g", "!.git" },
+					previewer = false,
+					layout_config = {
+						width = 0.8,
+						height = 0.7,
+					},
+				})
+			end, { noremap = true, silent = true })
 			vim.keymap.set("n", "<leader>phf", function()
 				require("telescope.builtin").find_files({
 					hidden = true,
 					no_ignore = true,
 				})
 			end, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>phF", function()
+				require("telescope.builtin").find_files({
+					hidden = true,
+					no_ignore = true,
+					previewer = false,
+					layout_config = {
+						width = 0.7,
+						height = 0.8,
+					},
+				})
+			end, { noremap = true, silent = true })
 			vim.keymap.set("n", "<C-g>", builtin.git_files, {})
 			vim.keymap.set("n", "<leader>gs", function()
 				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			end)
+			vim.keymap.set("n", "<leader>gS", function()
+				builtin.grep_string({
+					search = vim.fn.input("Grep > "),
+					previewer = false,
+					layout_config = {
+						width = 0.8,
+						height = 0.9,
+					},
+				})
 			end)
 			-- Use yanky with telescope
 			require("telescope").load_extension("yank_history")
