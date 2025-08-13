bfbafaefe6c5e49ddf80e9f26409d1674cce0bbd
Jedsam <fgokgoz02@hotmail.com>
2025-08-13 19:42:01 +0300

    some telescope fixes with window shape and wrapping etc

--- nvim/.config/nvim/lua/noru/plugins/lsp/conform-formatter.lua
+++ nvim/.config/nvim/lua/noru/plugins/lsp/conform-formatter.lua
@@ -1,39 +1,47 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					-- Conform will run multiple formatters sequentially
-					python = { "isort", "black" },
-					-- You can customize some of the format options for the filetype (:help conform.format)
-					rust = { "rustfmt", lsp_format = "fallback" },
-					-- Conform will run the first available formatter
-					javascript = { "prettierd", "prettier", stop_after_first = true },
-					c = { "astyle" },
-					cpp = { "clang-format" },
-					-- java = { "google-java-format" },
-				},
-				format_after_save = {
-					lsp_format = "fallback",
-				},
-				formatters = {
-					["clang-format"] = {
-						prepend_args = {
-							"--style={IndentWidth: 2, TabWidth: 2, UseTab: Never, ColumnLimit: 110, BinPackParameters: false, BinPackArguments: false, AllowAllParametersOfDeclarationOnNextLine: false, AlignAfterOpenBracket: AlwaysBreak, PenaltyBreakBeforeFirstCallParameter: 1, IndentAccessModifiers: false, AccessModifierOffset: -1, SpacesBeforeTrailingComments: 2, }",
-						},
-					},
-				},
-			})
-			-- Format on save
-			-- vim.api.nvim_create_autocmd("BufWritePre", {
-			-- 	pattern = "*",
-			-- 	callbac = function(args)
-			-- 		require("conform").format({ bufnr = args.buf, async = true })
-			-- 	end,
-			-- })
-		end,
-	},
+   {
+      "stevearc/conform.nvim",
+      opts = {},
+      config = function()
+         require("conform").setup({
+            formatters_by_ft = {
+               lua = { "stylua" },
+               -- Conform will run multiple formatters sequentially
+               python = { "isort", "black" },
+               -- You can customize some of the format options for the filetype (:help conform.format)
+               rust = { "rustfmt", lsp_format = "fallback" },
+               -- Conform will run the first available formatter
+               javascript = { "prettierd", "prettier", stop_after_first = true },
+               c = { "astyle" },
+               cpp = { "clang-format" },
+               -- java = { "google-java-format" },
+            },
+            format_after_save = {
+               lsp_format = "fallback",
+            },
+            formatters = {
+               ["stylua"] = {
+                  prepend_args = {
+                     "--indent-type",
+                     "Spaces",
+                     "--indent-width",
+                     "3",
+                  },
+               },
+               ["clang-format"] = {
+                  prepend_args = {
+                     "--style={IndentWidth: 2, TabWidth: 2, UseTab: Never, ColumnLimit: 110, BinPackParameters: false, BinPackArguments: false, AllowAllParametersOfDeclarationOnNextLine: false, AlignAfterOpenBracket: AlwaysBreak, PenaltyBreakBeforeFirstCallParameter: 1, IndentAccessModifiers: false, AccessModifierOffset: -1, SpacesBeforeTrailingComments: 2, SeparateDefinitionBlocks: Always, }",
+                  },
+               },
+            },
+         })
+         -- Format on save
+         -- vim.api.nvim_create_autocmd("BufWritePre", {
+         -- 	pattern = "*",
+         -- 	callbac = function(args)
+         -- 		require("conform").format({ bufnr = args.buf, async = true })
+         -- 	end,
+         -- })
+      end,
+   },
 }
--- nvim/.config/nvim/lua/noru/plugins/telescope.lua
+++ nvim/.config/nvim/lua/noru/plugins/telescope.lua
@@ -1,105 +1,129 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					sorting_strategy = "ascending",
-					layout_strategy = "horizontal",
-					layout_config = {
-						preview_width = 0.7,
-						width = 0.95,
-						height = 0.9,
-						prompt_position = "bottom",
-						preview_cutoff = 20,
-					},
-					hidden = true,
-					no_ignore = true,
-					file_ignore_patterns = {
-						"node_modules",
-						".ruff_cache",
-						".git/",
-						".mypy_cache",
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>pf", function()
-				require("telescope.builtin").find_files({
-					hidden = false,
-					find_command = { "rg", "--files", "-g", "!.git" },
-				})
-			end, { noremap = true, silent = true })
-			-- No preview project finder
-			vim.keymap.set("n", "<leader>pF", function()
-				require("telescope.builtin").find_files({
-					hidden = false,
-					find_command = { "rg", "--files", "-g", "!.git" },
-					previewer = false,
-					layout_config = {
-						width = 0.8,
-						height = 0.7,
-					},
-				})
-			end, { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>phf", function()
-				require("telescope.builtin").find_files({
-					hidden = true,
-					no_ignore = true,
-				})
-			end, { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>phF", function()
-				require("telescope.builtin").find_files({
-					hidden = true,
-					no_ignore = true,
-					previewer = false,
-					layout_config = {
-						width = 0.7,
-						height = 0.8,
-					},
-				})
-			end, { noremap = true, silent = true })
-			vim.keymap.set("n", "<C-g>", builtin.git_files, {})
-			vim.keymap.set("n", "<leader>gs", function()
-				builtin.grep_string({ search = vim.fn.input("Grep > ") })
-			end)
-			vim.keymap.set("n", "<leader>gS", function()
-				builtin.grep_string({
-					search = vim.fn.input("Grep > "),
-					previewer = false,
-					layout_config = {
-						width = 0.8,
-						height = 0.9,
-					},
-				})
-			end)
-			-- Use yanky with telescope
-			require("telescope").load_extension("yank_history")
-		end,
-	},
-	{
-		-- Plugin for code actions
-		"aznhe21/actions-preview.nvim",
-		config = function()
-			require("actions-preview").setup({
-				telescope = {
-					sorting_strategy = "ascending",
-					layout_strategy = "horizontal",
-					layout_config = {
-						width = 0.8,
-						preview_width = 0.4,
-						height = 0.9,
-						prompt_position = "top",
-						preview_cutoff = 20,
-						preview_height = function(_, _, max_lines)
-							return max_lines - 15
-						end,
-					},
-				},
-			})
-			vim.keymap.set({ "v", "n" }, "<leader>ca", require("actions-preview").code_actions)
-		end,
-	},
+   {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.8",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+         require("telescope").setup({
+            defaults = {
+               sorting_strategy = "ascending",
+               layout_strategy = "horizontal",
+               line_width = "full",
+               wrap_results = true,
+               wrap_line = true,
+               layout_config = {
+                  width = 0.99,
+                  height = 0.99,
+                  prompt_position = "bottom",
+                  preview_cutoff = 20,
+               },
+               layouts = {
+                  horizontal = {
+                     preview_width = 0.75,
+                  },
+               },
+               hidden = true,
+               no_ignore = true,
+               file_ignore_patterns = {
+                  "node_modules",
+                  ".ruff_cache",
+                  ".git/",
+                  ".mypy_cache",
+               },
+            },
+         })
+         -- Wrap lines in previewer
+         vim.api.nvim_create_autocmd("User", {
+            pattern = "TelescopePreviewerLoaded",
+            callback = function()
+               vim.wo.wrap = true
+               vim.wo.linebreak = true
+            end,
+         })
+         local builtin = require("telescope.builtin")
+         vim.keymap.set("n", "<leader>pf", function()
+            builtin.find_files({
+               hidden = false,
+               find_command = { "rg", "--files", "-g", "!.git" },
+            })
+         end, { noremap = true, silent = true })
+         -- No preview project finder
+         vim.keymap.set("n", "<leader>pF", function()
+            builtin.find_files({
+               hidden = false,
+               find_command = { "rg", "--files", "-g", "!.git" },
+               previewer = false,
+               layout_config = {
+                  width = 0.8,
+                  height = 0.7,
+               },
+            })
+         end, { noremap = true, silent = true })
+         vim.keymap.set("n", "<leader>phf", function()
+            builtin.find_files({
+               hidden = true,
+               no_ignore = true,
+            })
+         end, { noremap = true, silent = true })
+         vim.keymap.set("n", "<leader>phF", function()
+            builtin.find_files({
+               hidden = true,
+               no_ignore = true,
+               previewer = false,
+               layout_config = {
+                  width = 0.7,
+                  height = 0.8,
+               },
+            })
+         end, { noremap = true, silent = true })
+         vim.keymap.set("n", "<C-g>", builtin.git_files, {})
+         vim.keymap.set("n", "<leader>lgs", function()
+            builtin.live_grep({ search = vim.fn.input("Grep > ") })
+         end)
+         vim.keymap.set("n", "<leader>lgS", function()
+            builtin.live_grep({
+               search = vim.fn.input("Grep > "),
+               previewer = false,
+               layout_config = {
+                  width = 0.8,
+                  height = 0.9,
+               },
+            })
+         end)
+         vim.keymap.set("n", "<leader>gs", function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+         end)
+         vim.keymap.set("n", "<leader>gS", function()
+            builtin.grep_string({
+               search = vim.fn.input("Grep > "),
+               previewer = false,
+               layout_config = {
+                  width = 0.8,
+                  height = 0.9,
+               },
+            })
+         end)
+         -- Use yanky with telescope
+         require("telescope").load_extension("yank_history")
+      end,
+   },
+   {
+      -- Plugin for code actions
+      "aznhe21/actions-preview.nvim",
+      config = function()
+         require("actions-preview").setup({
+            telescope = {
+               sorting_strategy = "ascending",
+               layout_strategy = "vertical",
+               layout_config = {
+                  width = 0.99,
+                  height = 0.99,
+                  prompt_position = "top",
+                  preview_cutoff = 20,
+               },
+            },
+         })
+         vim.keymap.set({ "v", "n" }, "<leader>ca", require("actions-preview").code_actions)
+      end,
+   },
 }
