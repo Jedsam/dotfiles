35cf8310368086734cc402826ff93630eb0cc329
Jedsam <fgokgoz02@hotmail.com>
2025-08-25 20:51:46 +0300

    tabs now 2 space also changed trouble lsp to focus always active

--- nvim/.config/nvim/lua/noru/set.lua
+++ nvim/.config/nvim/lua/noru/set.lua
@@ -1,7 +1,7 @@
 -- tab settings
-vim.opt.tabstop = 4 -- Amount of spaces a \t character represent
-vim.opt.softtabstop = 4 -- Amount of spaces when pressing TAB or Backspace
-vim.opt.shiftwidth = 4 -- Amount of spaces used for indentation
+vim.opt.tabstop = 2 -- Amount of spaces a \t character represent
+vim.opt.softtabstop = 2 -- Amount of spaces when pressing TAB or Backspace
+vim.opt.shiftwidth = 2 -- Amount of spaces used for indentation
 vim.opt.expandtab = true -- Converts \t into spaces when pressing TAB
 
 -- Enable relative line numbers
