8b6095f393c656315e6c5df94162f7005bcae690
Jedsam <fgokgoz02@hotmail.com>
2025-08-28 13:31:59 +0300

    now pasting into visual mode selected text doesnt copy the deleted text

--- nvim/.config/nvim/lua/noru/remap.lua
+++ nvim/.config/nvim/lua/noru/remap.lua
@@ -3,8 +3,11 @@ vim.keymap.set("n", "<C-s>", vim.cmd.w) -- Save a file
 vim.keymap.set("i", "jk", "<ESC>", { silent = true }) -- Use jj in instert mode to swap back to normal mode
 
 -- Making delete permamently delete stuff and add new keybind for it (cut operation)
-vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "D", '"_D', { noremap = true, silent = true })
+vim.keymap.set({ "n", "x" }, "d", '"_d', { noremap = true, silent = true })
+vim.keymap.set({ "n", "x" }, "D", '"_D', { noremap = true, silent = true })
+-- Making paste permamently delete stuff instead of copying the deleted text in visual mode
+vim.keymap.set({ "x" }, "p", '"_dP', { noremap = true, silent = true })
+vim.keymap.set({ "x" }, "P", '"_dP', { noremap = true, silent = true })
 -- Making x delete stuff without messing with the clipboard
 vim.keymap.set({ "n", "v" }, "x", '"_x', { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "X", '"_X', { noremap = true, silent = true })
@@ -22,6 +25,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Smoother scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+vim.keymap.set("n", "n", "nzzzv", { noremap = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true })
 
 -- Add <leader> + x for Ctrl + X (decrease)
 vim.keymap.set("n", "<leader>x", "<C-x>", { noremap = true })
